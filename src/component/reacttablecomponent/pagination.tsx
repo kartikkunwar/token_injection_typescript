dbeb094b6c61e6f97573c7c6b17c87b025b33a21
kartik kunwar <kunwarkartik@gmail.com>
2023-12-22 13:42:16 +0530

    making pagination as a service

---
+++ src/component/reacttablecomponent/pagination.tsx
@@ -0,0 +1,10 @@
+
+
+
+export const Pagination=()=>{
+    return(
+        <div>
+            
+        </div>
+    )
+}
--- src/component/reacttablecomponent/table.tsx
+++ src/component/reacttablecomponent/table.tsx
@@ -10,11 +10,11 @@ interface ReactTableProps {
     columns: any,
     url: string,
     filter: object,
-    category:string
+    category: string
 }
 
 export const ReactTable = (props: ReactTableProps) => {
-    const { columns, url, filter, category} = props
+    const { columns, url, filter, category } = props
     const [data, setData] = useState([])
     const [filtereddata, setFiltereddata] = useState([])
     const [pages, setPages] = useState(1);
@@ -22,9 +22,9 @@ export const ReactTable = (props: ReactTableProps) => {
     const [perPage, setPerPage] = useState(10);
     const [search, setSearch] = useState("");
     const [beingSearched, setBeingSearched] = useState(false);
-    const [, setRowsSelected] = useState(false)
     const debounceSearch = useDebounce(search, 500)
     const [tableloader, setTableloader] = useState(false)
+    const [val, setVal] = useState(0)
 
     useEffect(() => {
         setTableloader(true)
@@ -49,49 +49,42 @@ export const ReactTable = (props: ReactTableProps) => {
                 .catch((err: any) => console.log(err))
         }
     }, [debounceSearch, pages, perPage, filter, url])
-    
 
-    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow,selectedFlatRows} = useTable(
+
+    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow, selectedFlatRows } = useTable(
         {
             columns,
             data: beingSearched ? filtereddata : data,
-            state:{
-                
+            state: {
+
             }
-        }, useSortBy, usePagination,useRowSelect,
-        (hooks)=>{hooks.visibleColumns.push((columns)=>{
-            return[
-                {
-                    id:"selection",
-                    Header:({getToggleAllRowsSelectedProps})=>(
-                        <TableCheckBox {...getToggleAllRowsSelectedProps()}/>
-                    ),
-                    Cell:({row})=>(
-                        <TableCheckBox {...row.getToggleRowSelectedProps()}/>
-                    )
-                },
-                ...columns
-            ]
-        })}
-        );
+        }, useSortBy, usePagination, useRowSelect,
+        (hooks) => {
+            hooks.visibleColumns.push((columns) => {
+                return [
+                    {
+                        id: "selection",
+                        Header: ({ getToggleAllRowsSelectedProps }) => (
+                            <TableCheckBox {...getToggleAllRowsSelectedProps()} />
+                        ),
+                        Cell: ({ row }) => (
+                            <TableCheckBox {...row.getToggleRowSelectedProps()} />
+                        )
+                    },
+                    ...columns
+                ]
+            })
+        }
+    );
 
-        // console.log(selectedFlatRows)
-
-    const pageRows=rows.slice(0,perPage)
-
-    const createPages=(n:number)=>{
-        return new Array(n).fill(0)
+    const handlepagechange = (e: any) => {
+        if (e.key === "Enter") {
+            if (val <= Math.ceil(totalRows / perPage)) {
+                setPages(val)
+            }
+        }
     }
 
-    const handlepage=(val:number)=>{
-        setPages(val)
-    }
-
-    useEffect(()=>{
-        
-    },[])
-
-   
     return (
         <div className="main">
             <div className="headers">
@@ -101,7 +94,7 @@ export const ReactTable = (props: ReactTableProps) => {
                         value={perPage}
                         onChange={(e) => setPerPage(Number(e.target.value))}
                     >
-                        {[10, 15, 20,25, 30].map((pageSize) => (
+                        {[10, 15, 20, 25, 30].map((pageSize) => (
                             <option key={pageSize} value={pageSize}>
                                 {pageSize}
                             </option>
@@ -117,96 +110,82 @@ export const ReactTable = (props: ReactTableProps) => {
                 </div>
             </div>
             {
-                tableloader&&<div className="loader"><Circles
-                height="80"
-                width="80"
-                color="#4fa94d"
-                ariaLabel="circles-loading"
-                wrapperStyle={{}}
-                wrapperClass=""
-                visible={true}
-            /></div>
+                tableloader && <div className="loader"><Circles
+                    height="80"
+                    width="80"
+                    color="#4fa94d"
+                    ariaLabel="circles-loading"
+                    wrapperStyle={{}}
+                    wrapperClass=""
+                    visible={true}
+                /></div>
             }
             {
-                !data.length&&!filtereddata.length&&!tableloader&&<h1 className="errormsg">There are no records to display</h1>
+                !data.length && !filtereddata.length && !tableloader && <h3 className="errormsg">There are no records to display</h3>
             }
             {
-                !tableloader&&(data.length!==0||filtereddata.length!==0)&&<div className="table">
-                <table {...getTableProps()}>
-                    <thead>
-                        {
-                            headerGroups.map(el => (
-                                <tr {...el.getHeaderGroupProps()}>
-                                    {
-                                        el.headers.map((column: any) => (
-                                            <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                                                {column.render("Header")}
-                                                {column.isSorted ? (
-                                                    column.isSortedDesc ? (
-                                                        <span>&darr;</span>
+                !tableloader && (data.length !== 0 || filtereddata.length !== 0) && <div className="table">
+                    <table {...getTableProps()}>
+                        <thead>
+                            {
+                                headerGroups.map(el => (
+                                    <tr {...el.getHeaderGroupProps()}>
+                                        {
+                                            el.headers.map((column: any) => (
+                                                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                                                    {column.render("Header")}
+                                                    {column.isSorted ? (
+                                                        column.isSortedDesc ? (
+                                                            <span>&darr;</span>
+                                                        ) : (
+                                                            <span>&uarr;</span>
+                                                        )
                                                     ) : (
-                                                        <span>&uarr;</span>
-                                                    )
-                                                ) : (
-                                                    ''
-                                                )}
-                                            </th>
-                                        ))
-                                    }
-                                </tr>
-                            ))
-                        }
-                    </thead>
-                    <tbody {...getTableBodyProps()}>
-                        {
-                            pageRows.map(row => {
-                                prepareRow(row)
-                                return <tr {...row.getRowProps()}  onClick={() => { console.log(' row click ', row)}}>
-                                    {
-                                        row.cells.map(cell => (
-                                            <td {...cell.getCellProps()}>
-                                                {cell.render("Cell")}
-                                            </td>
-                                        ))
-                                    }
-                                </tr>
-                            })
-                        }
-                    </tbody>
-                </table>
-            </div>
+                                                        ''
+                                                    )}
+                                                </th>
+                                            ))
+                                        }
+                                    </tr>
+                                ))
+                            }
+                        </thead>
+                        <tbody {...getTableBodyProps()}>
+                            {
+                                rows.map(row => {
+                                    prepareRow(row)
+                                    return <tr {...row.getRowProps()} onClick={() => { console.log(' row click ', row) }}>
+                                        {
+                                            row.cells.map(cell => (
+                                                <td {...cell.getCellProps()}>
+                                                    {cell.render("Cell")}
+                                                </td>
+                                            ))
+                                        }
+                                    </tr>
+                                })
+                            }
+                        </tbody>
+                    </table>
+                </div>
             }
             {
-                (data.length!==0||filtereddata.length!==0)&&<div className="paginationbutton">
-                <div>
-                    <span>Showing page {pages} of {Math.ceil(totalRows / perPage)}</span>
-                </div>
-                {/* <div>
-                    <button className="button" disabled={pages === 1} onClick={() => setPages(1)}>
-                        {'<<'}
-                    </button>{' '}
-                    <button className="button" disabled={pages === 1} onClick={() => setPages(pages - 1)}>
-                        {'<'}
-                    </button>{' '}
-                    <button className="button" disabled={pages === (Math.ceil(totalRows / perPage))} onClick={() => setPages(pages + 1)}>
-                        {'>'}
-                    </button>{' '}
-                    <button className="button" disabled={pages === (Math.ceil(totalRows / perPage))} onClick={() => setPages(Math.ceil(totalRows / perPage))}>
-                        {'>>'}
-                    </button>{' '}
-                </div> */}
+                (data.length !== 0 || filtereddata.length !== 0) && <div className="paginationbutton">
+                    <div>
+                        <span>Showing page {pages} of {Math.ceil(totalRows / perPage)}</span>
+                    </div>
+                    <ul className="pagination pagination-md justify-content-end">
+                        <div className="goto">
+                            <span>Go to Page: </span>
+                            <input type="number" className="input" placeholder={String(pages)} onChange={(e) => setVal(Number(e.target.value))} onKeyDown={handlepagechange} />
+                        </div>
+                        <li className="page-item"><button className="page-link butts" disabled={pages === 1} onClick={() => setPages(1)}>&laquo;</button></li>
+                        <li className="page-item"><button className="page-link butts" disabled={pages === 1} onClick={() => setPages(pages - 1)}>&lsaquo;</button></li>
+                        <li className="page-item"><button className="page-link butts" disabled={pages === (Math.ceil(totalRows / perPage))} onClick={() => setPages(pages + 1)}>&rsaquo;</button></li>
+                        <li className="page-item"><button className="page-link butts" disabled={pages === (Math.ceil(totalRows / perPage))} onClick={() => setPages(Math.ceil(totalRows / perPage))}>&raquo;</button></li>
 
-                <ul className="pagination pagination-md justify-content-end">
-                    <li className="page-item"><button className="page-link butts" disabled={pages === 1} onClick={() => setPages(1)}>&laquo;</button></li>
-                    <li className="page-item"><button className="page-link butts" disabled={pages === 1} onClick={() => setPages(pages - 1)}>&lsaquo;</button></li>
-                    {
-                        createPages(Math.ceil(totalRows / perPage)).map((a,i)=><li className="page-item" key={i}><button className={pages===i+1?"page-link btn btn-primary btn-lg active":"page-link "} onClick={()=>handlepage(i+1)}>{i+1}</button></li>)
-                    }
-                    <li className="page-item"><button className="page-link butts" disabled={pages === (Math.ceil(totalRows / perPage))} onClick={() => setPages(pages + 1)}>&rsaquo;</button></li>
-                    <li className="page-item"><button className="page-link butts" disabled={pages === (Math.ceil(totalRows / perPage))} onClick={() => setPages(Math.ceil(totalRows / perPage))}>&raquo;</button></li>
-                    
-                </ul>
-            </div>
+                    </ul>
+                </div>
             }
         </div>
     )
